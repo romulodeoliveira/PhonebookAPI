bb71d8fabca6e18a5f3f176ceea4a0a24c023e7c
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2025-03-28 13:05:50 -0300

    refactor: melhoria na estrutura do código

--- Models/RefreshToken.cs
+++
@@ -1,8 +0,0 @@
-namespace Phonebook.Models;
-
-public class RefreshToken
-{
-    public string Token { get; set; } = String.Empty;
-    public DateTime Created { get; set; } = DateTime.Now;
-    public DateTime Expires { get; set; }
-}
